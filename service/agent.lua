2edfcf325b3ec94d249162a9f66580afef220398
李红杰 <907801449@qq.com>
2018-09-14 14:06:48 +0800

    lib json

--- service/agent.lua
+++ service/agent.lua
@@ -1,9 +1,6 @@
 local skynet = require "skynet"
 local netpack = require "skynet.netpack"
 local socket = require "skynet.socket"
-local sproto = require "sproto"
-local sprotoloader = require "sprotoloader"
-local json = require "json.json"
 
 local WATCHDOG
 local host
@@ -18,25 +15,15 @@ skynet.register_protocol {
 	name = "client",
 	id = skynet.PTYPE_CLIENT,
 	unpack = function (msg, sz)
-		return host:dispatch(msg, sz)
+		return msg
 	end,
 	dispatch = function (fd, _, type, ...)
 		assert(fd == client_fd)	-- You can use fd to reply message
 		skynet.ignoreret()	-- session is fd, don't call skynet.ret
 		skynet.trace()
-		if type == "REQUEST" then
-			local ok, result  = pcall(request, ...)
-			if ok then
-				if result then
-					send_package(result)
-				end
-			else
-				skynet.error(result)
-			end
-		else
-			assert(type == "RESPONSE")
-			error "This example doesn't support request client"
-		end
+		
+		
+		skynet.error(...)
 	end
 }
 
@@ -44,16 +31,17 @@ function CMD.start(conf)
 	local fd = conf.client
 	local gate = conf.gate
 	WATCHDOG = conf.watchdog
-	-- slot 1,2 set at main.lua
-	host = sprotoloader.load(1):host "package"
-	send_request = host:attach(sprotoloader.load(2))
+
+
+	--[=[
 	skynet.fork(function()
 		while true do
 			send_package(send_request "heartbeat")
 			skynet.sleep(500)
 		end
 	end)
-
+	--]=]
+	
 	client_fd = fd
 	skynet.call(gate, "lua", "forward", fd)
 end
