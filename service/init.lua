811818f80f5d65f5e52b53c1a186ec8247f266ac
jack <907801449@qq.com>
2018-09-28 13:38:39 +0000

    add proto test function

--- service/init.lua
+++ service/init.lua
@@ -2,8 +2,30 @@ local skynet = require "skynet"
 
 local max_client = 64
 
+
+local function tproto()
+    protobuf = require "protobuf"
+    
+    root_path = skynet.getenv('root')
+
+    protobuf.register_file(root_path .. '../proto/test.pb')
+
+    stringbuffer = protobuf.encode("at",{
+        aa = 1222 
+    })
+
+
+    local data = protobuf.decode("at",stringbuffer)
+
+    skynet.error("proto test : " .. data.aa)
+
+end
+
 skynet.start(function()
-	skynet.error("Server start")
+
+    tproto()
+    
+    skynet.error("Server start")
 
     skynet.newservice("gameserver");
 
