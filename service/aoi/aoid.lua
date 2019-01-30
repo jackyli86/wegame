6513023b522a8bda0111d044f7e1915091ea68be
李红杰 <907801449@qq.com>
2019-01-30 18:36:09 +0800

    service aoi test

--- service/aoi/aoid.lua
+++ service/aoi/aoid.lua
@@ -70,7 +70,7 @@ end
 
 skynet.start(function()
     space = aoi.aoi_create();
-    setupvalue(aoi.aoi_message,1,lua_aoi_callback)
+    debug.setupvalue(aoi.aoi_message,1,lua_aoi_callback)
     skynet.fork(mainloop);
 
     skynet.name('.aoid',skynet.self())
